d55b38e41a254a6f1f7a6751a718eb6ad7f75177
NazaretsInfinity <157612924+NazaretsInfinity@users.noreply.github.com>
2025-08-18 18:49:30 +0500

    made it decimal

--- ASPmvc_Calculator/Controllers/CalculatorController.cs
+++ ASPmvc_Calculator/Controllers/CalculatorController.cs
@@ -6,15 +6,39 @@ namespace ASPmvc_Calculator.Controllers
     public class CalculatorController : Controller
     {
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(double? result, string? errorMessage)
         {
+            ViewBag.Result = result;
+            ViewBag.Error = errorMessage; 
             return View();
         }
 
         [HttpPost]
-        public IActionResult Calculate()
+        public IActionResult Calculate([FromForm] double num1, [FromForm] double num2, [FromForm] string operation)
         {
-            return View();
+            double result = 0;
+
+            try
+            {
+                switch (operation)
+                {
+                    case "+": result = num1 + num2; break;
+                    case "-": result = num1 - num2; break;
+                    case "*": result = num1 * num2; break;
+                    case "/":
+                        if (num2 == 0)
+                            throw new DivideByZeroException("Can't divide by zero");
+                        result = num1/num2; 
+                        break;
+                }
+
+                return RedirectToAction("Index", new { result });
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", new { errorMessage = ex.Message });
+            }
+
         }
     }
 }
